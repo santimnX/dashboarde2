9fece6207fd30bb67e1115eb57749227e0ede28b
ssanti04072008 <ssanti04072008@gmail.com>
2025-09-27 15:22:05 +0000

    ✨ feat(MoreDetails): Agrega sección de detalles     -Se crea una carpeta(ui) dentro de componentes     -Se introduce el nuevo componente `MoreDetails` para mostrar información adicional.

--- components/Profiledetails.tsx
+++ components/Profiledetails.tsx
@@ -1,20 +1,21 @@
 import "@/global.css"
 import React from 'react'
-import { Image, Text, View } from 'react-native'
-import IconButton from "./IconButton"
+import { Image, View } from 'react-native'
+import CustomText from "./ui/CustomText"
+import IconButton from "./ui/IconButton"
 
 const ProfileDetails = () => {
   return (
-    <View className="flex justify-center items-center">
+    <View className="flex justify-center items-center h-1/2 w-screen">
       <Image
         source={require('../assets/images/icon.png')}
         className="max-h-36 max-w-36 rounded-full"
       />
-      <Text className="text-2xl font-bold"> SANTIGO MOLINA </Text>
-      <Text className="text-xl font-bold text-gray-400"> ESTUDIANTE</Text>
-
+      <CustomText variant="title" value="Milton Velasquez"/>
+      <CustomText variant="subtitle" value="Profesor"/>
+      
       <View className="flex-row justify-center mt-4 gap-4">
-        <IconButton icon="logo-instagram" pulsar={() => console.log("Instagram")} color="pink"></IconButton>
+        <IconButton icon="logo-instagram" pulsar={() => console.log("Instagram")} color="red"></IconButton>
         <IconButton icon="logo-whatsapp" pulsar={() => console.log("Whatsapp")} color="green"></IconButton>
         <IconButton icon="logo-facebook" pulsar={() => console.log("Facebook")} color="blue"></IconButton>
         <IconButton icon="logo-linkedin" pulsar={() => console.log("Linkedin")} color="lightblue"></IconButton>
