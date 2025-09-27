98c6bed3da1b71cbdc9ac4945de0932aa00cd141
ssanti04072008 <ssanti04072008@gmail.com>
2025-09-27 15:14:28 +0000

    ✨feat(components): Se agrega una nueva funcion     -Una nueva fila llamada IconButton     -Se arregla mejor los file de "CustomText" y "Profiledetails"

--- components/Profiledetails.tsx
+++ components/Profiledetails.tsx
@@ -10,11 +10,11 @@ const ProfileDetails = () => {
         source={require('../assets/images/icon.png')}
         className="max-h-36 max-w-36 rounded-full"
       />
-      <Text className="text-2xl font-bold">Milton Velasquez</Text>
-      <Text className="text-xl font-bold text-gray-400">Profesor</Text>
+      <Text className="text-2xl font-bold"> SANTIGO MOLINA </Text>
+      <Text className="text-xl font-bold text-gray-400"> ESTUDIANTE</Text>
 
       <View className="flex-row justify-center mt-4 gap-4">
-        <IconButton icon="logo-instagram" pulsar={() => console.log("Instagram")} color="red"></IconButton>
+        <IconButton icon="logo-instagram" pulsar={() => console.log("Instagram")} color="pink"></IconButton>
         <IconButton icon="logo-whatsapp" pulsar={() => console.log("Whatsapp")} color="green"></IconButton>
         <IconButton icon="logo-facebook" pulsar={() => console.log("Facebook")} color="blue"></IconButton>
         <IconButton icon="logo-linkedin" pulsar={() => console.log("Linkedin")} color="lightblue"></IconButton>
